5bcd406f300b0669b95f884181df354aa7165e00
bulatie <bulatie@gmail.com>
2014-05-16 12:24:15 +0800

    add scheduling module

---
+++ packages/scheduling/public/config/routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mean').config(['$stateProvider',
+    function($stateProvider) {
+        $stateProvider.state('scheduling example page', {
+            url: '/scheduling/example',
+            templateUrl: 'scheduling/views/index.html'
+        });
+    }
+]);
---
+++ packages/scheduling/server/routes/scheduling.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// The Package is past automatically as first parameter
+module.exports = function(Scheduling, app, auth, database) {
+
+    app.get('/scheduling/example/anyone', function(req, res, next) {
+        res.send('Anyone can access this');
+    });
+
+    app.get('/scheduling/example/auth', auth.requiresLogin, function(req, res, next) {
+        res.send('Only authenticated users can access this');
+    });
+
+    app.get('/scheduling/example/admin', auth.requiresAdmin, function(req, res, next) {
+        res.send('Only users with Admin role can access this');
+    });
+
+    app.get('/scheduling/example/render', function(req, res, next) {
+        Scheduling.render('index', {
+            package: 'scheduling'
+        }, function(err, html) {
+            //Rendering a view from the Package server/views
+            res.send(html);
+        });
+    });
+};
