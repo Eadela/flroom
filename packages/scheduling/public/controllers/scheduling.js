e999fa8d6deee5e14f5106949d6c14723b519bbf
bulatie <bulatie@gmail.com>
2014-06-18 03:42:27 +0800

    规定了日程数据结构为三维数组，实现根据数据显示课程，可以添加课程，但是一个节只能添加一个（bug）

--- packages/scheduling/public/controllers/scheduling.js
+++ packages/scheduling/public/controllers/scheduling.js
@@ -3,15 +3,305 @@
 angular.module('mean').controller('SchedulingController', ['$scope', 'Global', '$http',
     function($scope, Global, $http) {
         $scope.global = Global;
-        $scope.abs = 'bbc';
         $scope.popover = {
             'title': ' ',
             'content': ' '
         };
-        $scope.d3w6 = {
-        	className: 'abc',
-        	startWeek: '12',
-        	endWeek: '15'
+        $scope.classInf = {};
+        $scope.classIndex = [];
+        $scope.dateOfDayInit = function(schedules, index){
+          return schedules.map(function(schedule){
+            schedule[0].dateOfDay = index;
+          });
         };
+        $scope.add = function(classes, classInf) {
+            var newclass = {};
+            Object.defineProperties(newclass, {
+                classname: {
+                    value: classInf.classname,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                },
+                dateOfDay: {
+                    value: classInf.dateOfDay,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                },
+                dayOfWeek: {
+                    value: classInf.dayOfWeek,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                },
+                endWeek: {
+                    value: classInf.endWeek,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                },
+                startWeek: {
+                    value: classInf.startWeek,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                }
+            });
+            $scope.classInf = {};
+            return classes.push(newclass);
+        };
+        $scope.schedules = [
+            [
+                [{
+                    classname: '程序设计语言原理',
+                    endWeek: 9,
+                    startWeek: 1,
+                }, {
+                    classname: '程序设计语言原理',
+                    endWeek: 9,
+                    startWeek: 1,
+                }, {
+                    classname: '程序设计语言原理',
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }]
+            ],
+            [
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }]
+            ],
+            [
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }]
+            ],
+            [
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }]
+            ],
+            [
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }],
+                [{
+                    classname: '程序设计语言原理',
+                    dateOfDay: 1,
+                    dayOfWeek: 1,
+                    endWeek: 9,
+                    startWeek: 1,
+                }]
+            ]
+        ];
     }
 ]);
